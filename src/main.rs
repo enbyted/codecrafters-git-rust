cb84806fb8c028dbce6511005c73b006204d3159
Bartosz Grabias <bartosz.grabias@enbyted.pl>
2023-12-02 17:50:00 +0000

    Implement object file reading

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,7 @@ use clap::{
     builder::{ValueParser, ValueParserFactory},
     Args, Parser,
 };
-use std::{
-    fs::{self, FileType},
-    path::PathBuf,
-};
+use std::{fs, io::Read, io::Write, path::PathBuf};
 
 #[derive(Parser)]
 enum Subcommand {
@@ -33,6 +30,14 @@ impl ObjectRef {
         anyhow::ensure!(hash.len() == 40);
         anyhow::ensure!(hash.chars().all(|c| c.is_ascii_hexdigit()));
         Ok(ObjectRef(hash.to_owned()))
+    }
+
+    fn hash_prefix(&self) -> &str {
+        &self.0[..2]
+    }
+
+    fn matches_remainder(&self, remainder: &str) -> bool {
+        remainder.eq_ignore_ascii_case(&self.0[2..])
     }
 }
 
@@ -90,6 +95,82 @@ impl Repository {
         fs::write(self.path.join("HEAD"), "ref: refs/heads/master\n")?;
         Ok(())
     }
+
+    pub fn find_object(&self, object_ref: &ObjectRef) -> anyhow::Result<Object> {
+        let maybe_container_path = fs::read_dir(self.path.join("objects/"))?.find_map(|f| {
+            let dir = f.ok()?;
+            if dir.file_type().ok()?.is_dir()
+                && dir
+                    .file_name()
+                    .eq_ignore_ascii_case(object_ref.hash_prefix())
+            {
+                Some(dir.path())
+            } else {
+                None
+            }
+        });
+
+        if let Some(container_path) = maybe_container_path {
+            let maybe_object_path = fs::read_dir(container_path)?.find_map(|f| {
+                let file = f.ok()?;
+                if file.file_type().ok()?.is_file()
+                    && object_ref.matches_remainder(file.file_name().to_str()?)
+                {
+                    Some(file.path())
+                } else {
+                    None
+                }
+            });
+
+            if let Some(object_path) = maybe_object_path {
+                Object::from_path(&object_path)
+            } else {
+                Err(anyhow::Error::msg("Could not find requested object"))
+            }
+        } else {
+            Err(anyhow::Error::msg("Could not find requested object"))
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Object {
+    path: PathBuf,
+    contents: Vec<u8>,
+    kind: String,
+}
+
+impl Object {
+    fn from_path(path: &PathBuf) -> anyhow::Result<Object> {
+        let file = fs::File::open(path)?;
+        let mut decoder = flate2::read::ZlibDecoder::new(&file);
+        let mut contents = Vec::new();
+        decoder.read_to_end(&mut contents)?;
+
+        // The data in contents is structured like this:
+        // <type> <size>\0<payload>
+        //       ^- a space character
+        // Thus we need to split at first zero byte
+        let index_first_zero = contents.iter().position(|b| *b == 0u8).ok_or_else(|| {
+            anyhow::Error::msg("Invalid object file data, could not find header terminator.")
+        })?;
+        let header = std::str::from_utf8(&contents[..index_first_zero])?;
+        let (object_type, object_size) = header.split_once(' ').ok_or_else(|| {
+            anyhow::Error::msg(format!(
+                "Invalid object file header format, expected '<type> <size>', found '{}'",
+                header
+            ))
+        })?;
+        let object_data = &contents[index_first_zero + 1..];
+        let object_size = object_size.parse::<usize>()?;
+        anyhow::ensure!(object_size == object_data.len());
+
+        Ok(Object {
+            path: path.clone(),
+            kind: object_type.to_owned(),
+            contents: object_data.to_owned(),
+        })
+    }
 }
 
 fn cmd_init() -> anyhow::Result<()> {
@@ -100,8 +181,11 @@ fn cmd_init() -> anyhow::Result<()> {
 
 fn cmd_cat_file(args: CatFileArgs) -> anyhow::Result<()> {
     let repo = Repository::find_from_current_dir()?;
-    println!("Git repository found in {:?}", repo.path);
-    todo!("Search for object {:?}", args.object);
+    eprintln!("Git repository found in {:?}", repo.path);
+    let obj = repo.find_object(&args.object)?;
+    eprintln!("Found object: {:?}", obj);
+    std::io::stdout().lock().write_all(&obj.contents)?;
+    Ok(())
 }
 
 fn main() {
